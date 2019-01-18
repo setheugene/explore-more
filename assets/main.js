5d2a9549d7a5a26c7d8128ee1c34de8a65c33a96
setheugene <seth.eugene@icloud.com>
2019-01-17 20:02:25 -0600

    random spinner

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,33 @@ var tomorrow = moment().add(1, "days").format("YYYY-MM-DD");
 $("#start-date").val(today);
 $("#end-date").val(tomorrow);
 
+
+$(document).ready(function() {
+   
+        
+    var places = ["Denver", "New York", "Portland", "Washington DC", "Houston"]
+
+    var random = places[Math.floor(Math.random() * places.length)];
+    console.log(random);
+
+
+    $("#spin").append(random);
+    $("#spin").addClass("hidden");
+
+var elements = document.querySelectorAll('#spin');
+
+Array.prototype.forEach.call(elements, function (el) {
+  const chaffle = new Chaffle(el, { /* options */ });
+  spinb.addEventListener('click', function () {
+    $("#spin").addClass("show");
+    chaffle.init();
+    
+  });
+});
+})
+
+
+// Array.prototype.forEach.call(elements, function (el) {
+//   var chaffle = new Chaffle(el, { /* options */ });
+//   chaffle.init();
+// });
