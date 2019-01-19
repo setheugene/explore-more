7b5b89f39c5f5d43d32631815c68aead3562088e
JakeD <43915365+jakeD9@users.noreply.github.com>
2019-01-19 11:01:31 -0600

    adjusted bootstrap food table, appends zomato data, also put the entire JS under the click handler for the spin button

--- assets/main.js
+++ assets/main.js
@@ -3,23 +3,47 @@ var tomorrow = moment().add(1, "days");
 
 $(document).ready(function () {
 
-  var places = ["Denver", "New York", "Portland", "Washington DC", "Houston"]
+  // hiding the div at first
+  $("#spin").addClass("hidden");
+
+  // click handler so that nothing runs without it being clicked first
+  $("#spin-btn").on("click", function () {
+
+  var places = ["Denver", "Jacksonville", "Portland", "Kansas City", "Las Vegas"]
 
   var random = places[Math.floor(Math.random() * places.length)];
   console.log(random);
 
+  // function to get the zomato city id code assigned to the randomly chosen city
+  function placeChosen() {
+    if (random === "Denver") {
+      return "305";
+    } else if (random === "Jacksonville") {
+      return "574";
+    } else if (random === "Portland") {
+      return "286";
+    } else if (random === "Kansas City") {
+      return "856";
+    } else if (random === "Las Vegas") {
+      return "282";
+    }
+  };
+
+  // zomato ajax elements
+  var zomatoKey = "af0b75e10ec2c9e797c35598e8fc0207";
+  var zomatoPlace = placeChosen();
+  console.log(zomatoPlace);
+
+  var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + zomatoPlace + "&entity_type=city&count=5&sort=rating";
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + random + "&cnt=5&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
 
   $("#start-date").val(today.format("YYYY-MM-DD"));
   $("#end-date").val(tomorrow.format("YYYY-MM-DD"));
-
-  $("#spin").append(random);
-  $("#spin").addClass("hidden");
-
-  $("#spin-btn").on("click", function () {
- 
+  $("#spin").text(random);
+  
     $("#spin").addClass("show");
     $("#weather-table").empty();
+    $("#food-table").empty();
 
     // Weather api call and displaying results to table
     $.ajax({
@@ -37,7 +61,40 @@ $(document).ready(function () {
         $("#weather-table").append(newRow);
       })
     });
+    
+    // zomato api call and dispalying results to table
+    console.log(zomatoQueryURL);
+    $.ajax({
+      url: zomatoQueryURL,
+      method: "GET",
+      headers: {
+        'user-key': zomatoKey
+      },
+    }).then(function(response) {
+      console.log(response);
+      // function to pull the proper info from the array
+        for (i = 0; i < response.restaurants.length; i++) {
+          var information = {
+              name: response.restaurants[i].restaurant.name, 
+              rating: response.restaurants[i].restaurant.user_rating.aggregate_rating, 
+              type: response.restaurants[i].restaurant.cuisines,
+              link: response.restaurants[i].restaurant.events_url,
+            };
+          console.log(information.name);
+          console.log(information.rating);
+          console.log(information.type);
+          console.log(information.link);
 
+          var foodRow = $("<tr>");
+          foodRow.append([
+            $("<td>").append(information.name),
+            $("<td>").append(information.rating),
+            $("<td>").append(information.type),
+            $("<td>").append("<a href=" + information.link + " target=_blank>Zomato Page</a>"),
+          ]);
+          $("#food-table").append(foodRow);
+        }
+    });
   });
 })
 
