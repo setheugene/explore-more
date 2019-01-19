cded7c9bbef886753a195609bc01daa6e682cba1
Curtis Simpson <crsimpson5@gmail.com>
2019-01-19 12:08:20 -0600

    added map

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,66 @@
 var today = moment();
 var tomorrow = moment().add(1, "days");
 
+var map;
+
+function initializeMap(city) {
+  $("#map").empty();
+
+  // Initialize the Platform object for maps
+  var platform = new H.service.Platform({
+    'app_id': 'Ck0H4ECXsoZw2kJ1JNpK',
+    'app_code': 'V7OoN7FBZLHPyoQ-8Uh9uQ',
+    useHTTPS: true
+  });
+
+  var pixelRatio = window.devicePixelRatio || 1;
+  var defaultLayers = platform.createDefaultLayers({
+    tileSize: pixelRatio === 1 ? 256 : 512,
+    ppi: pixelRatio === 1 ? undefined : 320
+  });
+
+  // Initialize map
+  map = new H.Map(document.getElementById('map'),
+    defaultLayers.normal.map, { pixelRatio: pixelRatio });
+
+  // Make the map interactive
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+  // Create the parameters for the geocoding request:
+  var geocodingParams = {
+    searchText: city
+  };
+
+  // Define a callback function to process the geocoding response:
+  var onResult = function (result) {
+    var locations = result.Response.View[0].Result,
+      position,
+      marker;
+
+    position = {
+      lat: locations[0].Location.DisplayPosition.Latitude,
+      lng: locations[0].Location.DisplayPosition.Longitude
+    };
+
+    moveMap(map, position.lat, position.lng);
+  };
+
+  // Get an instance of the geocoding service:
+  var geocoder = platform.getGeocodingService();
+
+  geocoder.geocode(geocodingParams, onResult, function (e) {
+    alert(e);
+  });
+}
+
+function moveMap(map, lat, lng) {
+  map.setCenter({ lat: lat, lng: lng });
+  map.setZoom(12);
+}
+
 $(document).ready(function () {
 
   // hiding the div at first
@@ -9,39 +69,42 @@ $(document).ready(function () {
   // click handler so that nothing runs without it being clicked first
   $("#spin-btn").on("click", function () {
 
-  var places = ["Denver", "Jacksonville", "Portland", "Kansas City", "Las Vegas"]
+    var places = ["Denver", "Jacksonville", "Portland", "Kansas City", "Las Vegas"]
 
-  var random = places[Math.floor(Math.random() * places.length)];
-  console.log(random);
+    var random = places[Math.floor(Math.random() * places.length)];
+    console.log(random);
 
-  // function to get the zomato city id code assigned to the randomly chosen city
-  function placeChosen() {
-    if (random === "Denver") {
-      return "305";
-    } else if (random === "Jacksonville") {
-      return "574";
-    } else if (random === "Portland") {
-      return "286";
-    } else if (random === "Kansas City") {
-      return "856";
-    } else if (random === "Las Vegas") {
-      return "282";
-    }
-  };
+    // function to get the zomato city id code assigned to the randomly chosen city
+    function placeChosen() {
+      if (random === "Denver") {
+        return "305";
+      } else if (random === "Jacksonville") {
+        return "574";
+      } else if (random === "Portland") {
+        return "286";
+      } else if (random === "Kansas City") {
+        return "856";
+      } else if (random === "Las Vegas") {
+        return "282";
+      }
+    };
 
-  // zomato ajax elements
-  var zomatoKey = "af0b75e10ec2c9e797c35598e8fc0207";
-  var zomatoPlace = placeChosen();
-  console.log(zomatoPlace);
+    // zomato ajax elements
+    var zomatoKey = "af0b75e10ec2c9e797c35598e8fc0207";
+    var zomatoPlace = placeChosen();
+    console.log(zomatoPlace);
 
-  var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + zomatoPlace + "&entity_type=city&count=5&sort=rating";
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + random + "&cnt=5&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
+    var zomatoQueryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=" + zomatoPlace + "&entity_type=city&count=5&sort=rating";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + random + "&cnt=5&units=imperial&APPID=166a433c57516f51dfab1f7edaed8413";
 
-  $("#start-date").val(today.format("YYYY-MM-DD"));
-  $("#end-date").val(tomorrow.format("YYYY-MM-DD"));
-  $("#spin").text(random);
-  
+    initializeMap(random);
+
+    $("#start-date").val(today.format("YYYY-MM-DD"));
+    $("#end-date").val(tomorrow.format("YYYY-MM-DD"));
+
+    $("#spin").append(random);
     $("#spin").addClass("show");
+
     $("#weather-table").empty();
     $("#food-table").empty();
 
@@ -61,7 +124,7 @@ $(document).ready(function () {
         $("#weather-table").append(newRow);
       })
     });
-    
+
     // zomato api call and dispalying results to table
     console.log(zomatoQueryURL);
     $.ajax({
@@ -97,7 +160,6 @@ $(document).ready(function () {
     });
   });
 })
-
 
 // Array.prototype.forEach.call(elements, function (el) {
 //   var chaffle = new Chaffle(el, { /* options */ });
